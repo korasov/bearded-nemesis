3b40fd381e36a3730ed91183bd28c2696f48d437
k0stu <korasov@gmail.com>
2012-06-12 15:31:39 +0300

    Conf and Comm fix

--- gameserver/src/kvgameserver/Communicator.java
+++ gameserver/src/kvgameserver/Communicator.java
@@ -1,29 +1,68 @@
 package kvgameserver;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import kvgameserver.service.Configuration;
+
 public class Communicator implements Runnable {
 
 	private Socket socket = null;
+	private BufferedReader br = null;
+	private OutputStream os = null;
+	private String games = null; 
 
 	public Communicator(Socket socket) {
 		this.socket = socket;
+		games = Configuration.getInstance().get("games");
+		try {
+			br = new BufferedReader(new InputStreamReader(
+					this.socket.getInputStream()));
+			os = this.socket.getOutputStream();
+		} catch (IOException e) {
+			this.print("Error getting streams from socket. Shutting down.");
+			System.exit(1);
+		}
+		
+	}
+
+	private void print(String string) {
+		System.out.println(string);
 	}
 
 	public void run() {
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					socket.getInputStream()));
-			OutputStream os = socket.getOutputStream();
-			while(!socket.isClosed()) {
-				String message = br.readLine();
-				String respond = "RE: " + message;
-				os.write(respond.getBytes());
+			String message = null;
+			while((message = br.readLine()) != null) {
+				String[] parts = message.split(":");
+				String command = parts[0].trim().toLowerCase();
+				this.print(command);
+				switch (command) {
+				case "conn" :
+					this.connectPlayer(parts[1].trim());
+				}
 			}
 		} catch (Exception e) { e.printStackTrace(); }
 	}
 
+	private void connectPlayer(String playerName) throws IOException {
+		if (playerName != null) {
+			DataKeeper.players.add(playerName);
+			this.print(playerName + " connected.");
+			StringBuilder playersOnline = new StringBuilder();
+			for (String name : DataKeeper.players) {
+				if (!name.equals(playerName)) {
+					playersOnline.append(name).append(" ");
+				}
+			}
+			String sPlayers = "PLAYERS: " + playersOnline.toString() + "\n";
+			String sGames = "GAMES: " + games + "\n";
+			os.write(sPlayers.getBytes());
+			// TODO: uncomment here when multiple games implemented:
+			// os.write(sGames.getBytes());
+		}
+	}
 }
