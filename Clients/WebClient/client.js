eacfe1a6c12630997ac6a629f34d7520d7a05fb6
ПК <@192.168.1.4>
2012-06-20 11:24:01 +0300

    one-page client

--- Clients/WebClient/client.js
+++ Clients/WebClient/client.js
@@ -30,6 +30,7 @@ function onClose(evt)
 
 var command;
 var inMessage;
+
 //принятие и оработка запроса
 function onMessage(evt)
 {
@@ -37,16 +38,15 @@ function onMessage(evt)
     var arr = mess.split(':');
 
     command= arr[0].toLowerCase();
-    if (inMessage!=null)
+    if (arr.length>1)
         inMessage=arr[1].trim();
 
     switch(command)  {
         case "players": initPersList(inMessage); break;
-        case "offer": //Offer message
-            break;
+        case "offer": sendOffer(); break;
         case "start": newGame(inMessage); break;
         case "status":  gameOver(inMessage); break;
-        case "rejected": writeToScreen("Choose another player"); break;
+        case "rejected": alert("Choose another player"); break;
         case "put":  put(inMessage);break;
         default: alert("Unknown command was sent. It was "+command); break;
     }
@@ -74,41 +74,65 @@ function writeToScreen(message)
 
 function getName(){
     var name = document.getElementById("inp").value;
-    alert(name);
     doSend("CONN:"+name);
     doSend("players");
-    location.replace('ChoosePlayer.html');
+    deleteRows(3) ;
+    addPlayerList();
+
 }
 //----------------выбор соперника
 
 function setPlayer(){
-    var plaeyr = GetSelectedItem(); //добаить в выбранному элементу ID player
-    doSend("OPPONENT:"+plaeyr);
-    location.replace('WebView.html');
+    var player = GetSelectedItem();
+    doSend("OPPONENT:"+player);
+  }
+
+function playerChoise(){
+    deleteRows(3) ;
 }
 
+
+function deleteRows(num) {
+    for(var i=0;i<num;i++){
+    var table = document.getElementById('editable').deleteRow(-1);
+    }
+   }
+
+var array;
+
 function initPersList(inMessage){
-    var array = inMessage.split(" ");
-    if(array.length==0)  alert("Вы единственный игрок. Ждите.")
+
+    var table = document.getElementById('editable');
+//    var title = document.createElement("p");
+//    title.innerHTML = "Choose player";
+//    table.appendChild(title);
+
+    array =inMessage.split(" ");
+
+    if(array[0]=="") {
+        alert("Вы единственный игрок. Ждите."); }
+     else{
     var inputRadio="";
     for ( keyVar in array ) {
 
         inputRadio += "<input name='rad' type='radio' id=''"+ array[keyVar]+"'>"+array[keyVar]+"<br>"
       }
-    document.getElementById("namelist").innerHTML=inputRadio
+    document.getElementById('namelist').innerHTML=inputRadio
+
+
+    var button = document.createElement("p");
+    button.innerHTML = "<input name='Submit' type='Button' value='Submit' onclick='setPlayer()'>";
+    table.appendChild(button); }
 }
 
 function GetSelectedItem() {
-    var chosen = ""
-     var len = document.list.rad.length
-    //alert("length"+len);
-
-    for (i = 0; i <len; i++) {
+    var chosen = ""  ;
+     for (i = 0; i <array.length; i++) {
         if (document.list.rad[i].checked) {
             chosen = array[i];
         }
     }
-return chosen;
+    return chosen;
 }
 
 //--------------сессия игры
@@ -125,7 +149,28 @@ function newGame(inMessage){
         mark = "O";
         mark2="X";
     }
-   }
+  var table = document.getElementById('editable');
+  table.innerHTML="<tr>  <td style='border: 4px solid green;' width='70' height='70' id='00'  align='center' onclick=' alert(\"Yf\");'> </td> " +
+      "  <td  style='border: 4px solid green;' width='70' height='70' id='01'  align='center'> </td>" +
+      " <td style='border: 4px solid green;' width='70' height='70' id='02'  align='center'> </td>  </tr>" +
+
+
+        " <tr> <td style='border: 4px solid green;' width='70' height='70' id='10'  align='center'> </td> " +
+        "  <td style='border: 4px solid green;' width='70' height='70' id='11'  align='center'> </td> " +
+            "  <td style='border: 4px solid green;' width='70' height='70' id='12'  align='center'> </td> </tr>" +
+
+
+        " <tr>  <td style='border: 4px solid green;' width='70' height='70' id='20'  align='center'> </td> " +
+        "  <td style='border: 4px solid green;' width='70' height='70' id='21'  align='center'> </td> " +
+            " <td style='border: 4px solid green;' width='70' height='70' id='22'  align='center'> </td> </tr>";
+
+table.style.border="4px solid white border-collapse: collapse;" ;
+    table.align="center";
+}
+function go(){
+    alert("Yf");
+}
+
 function put(inMessage)  {
      var cell=document.getElementById(inMessage);
 
@@ -133,14 +178,15 @@ function put(inMessage)  {
 }
 
 $(document).ready(function(){
-    $('table#game td').click(function(e){
+    $('table#editable td').click(function(e){
         var t = e.target || e.srcElement;
-
+        alert(t.id);
        //получаем подтверждение хода
         if(turn){
             if(cell.innerHTML=="O"||cell.innerHTML=="X") {alert ("You can't put mark here!")}
            else{
-            doSend("PLAYER:"+ t.id);
+                alert(t.id);
+            doSend("PUT:"+ t.id);
             t.innerHTML=mark; turn=false;}
         }
         else alert("Wait for your turn");
@@ -153,28 +199,16 @@ function gameOver(inMessage){
          //вызывается предложение сиграть сново
 }
 //окно предложить играть сново
-$(function(){
-    $('#dialog1').dialog({
-        autoOpen: false,
-//position: ["center","center"],
-        width: 300,
-//height: 70,
-        title: "{Хотите сиграть с этим игроком сново?",
-        modal: true,
-        buttons: {
-            "Да": function() {
-                alert("Играем сново");
-                //переход в начало игры
-
-            },
-            "Нет": function(){alert("Выбор соперника");
-                //переход к выбору соперника
-            }
-        }
-    });
-})
+//<form name=myform>
+//    <input type=button value="Try it now"
+//    onClick="if(confirm('Do you want to play with..?'))
+//startGame();">
+//    </form>
 
 
+function sendOffer(){
 
+if(confirm('Do you want to play with..?'))  doSend("OFFERRESPONSE: YES");
+else  doSend("OFFERRESPONSE: NO"); }
 
 
